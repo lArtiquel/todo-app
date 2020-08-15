d392ada678d2f3743677a1fe3e840739086c03d7
Artyom <artem.tsvirko@mail.ru>
2020-08-15 21:59:46 +0300

    Updates

---
+++ precache-manifest.eee4c19f5ee6a0eda11babff59910e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdb1a76705e2d18bc48104cc2d86fa05",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "f2d706c5caab166d7d4e",
+    "url": "/todo-app/static/css/main.9cc37ddb.chunk.css"
+  },
+  {
+    "revision": "1827f290dc4df90d0cd4",
+    "url": "/todo-app/static/js/2.1049b270.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todo-app/static/js/2.1049b270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d706c5caab166d7d4e",
+    "url": "/todo-app/static/js/main.2da14747.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  }
+]);

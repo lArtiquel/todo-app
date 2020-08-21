ebca8286a55f5b0a02a62e5b9ee701cdc0abcd98
Artyom <artem.tsvirko@mail.ru>
2020-08-21 23:50:09 +0300

    Move constants to the separate class.

---
+++ backend/src/main/java/com/todo/security/jwt/JwtConstants.java
@@ -0,0 +1,41 @@
+package com.todo.security.jwt;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class JwtConstants {
+
+    @Value("${app.config.jwt.access.header}")
+    private String accessJwtHeaderName;
+
+    @Value("${app.config.jwt.access.claim.userId}")
+    private String accessJwtUserIdClaimName;
+
+    @Value("${app.config.jwt.access.claim.authorities}")
+    private String accessJwtAuthoritiesClaimName;
+
+    @Value("${app.config.jwt.access.secret}")
+    private String accessJwtSecret;
+
+    @Value("${app.config.jwt.access.expirationTimeInSec}")
+    private Integer accessJwtExpirationTimeInSec;
+
+    @Value("${app.config.jwt.refresh.secret}")
+    private String refreshJwtSecret;
+
+    @Value("${app.config.jwt.refresh.expirationTimeInSec}")
+    private Integer refreshJwtExpirationTimeInSec;
+
+    @Value("${app.config.jwt.emailVerification.secret}")
+    private String emailVerificationJwtSecret;
+
+    @Value("${app.config.jwt.passwordRestoring.secret}")
+    private String passwordRestoringJwtSecret;
+
+    @Value("${app.config.jwt.passwordRestoring.expirationTimeInSec}")
+    private Integer passwordRestoringJwtExpTimeInSec;
+
+}

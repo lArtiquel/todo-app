b31e58d5d52ec4bf1703384a0c45530be4ac0b21
Artyom <artem.tsvirko@mail.ru>
2020-08-23 23:52:15 +0300

    Update user logged in event and listener.

--- backend/src/main/java/com/todo/event/OnUserLoggedInEvent.java
+++ backend/src/main/java/com/todo/event/OnUserLoggedInEvent.java
@@ -1,26 +1,16 @@
 package com.todo.event;
 
-import com.todo.security.UserDetailsImpl;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
-import org.springframework.security.core.userdetails.UserDetails;
 
+@Getter
 public class OnUserLoggedInEvent extends ApplicationEvent {
 
-    private UserDetailsImpl userDetails;
+    private String userId;
 
-    /**
-     * @param userDetails passing user details of currently logged in user to the event
-     */
-    public OnUserLoggedInEvent(UserDetailsImpl userDetails) {
-        super(userDetails);
-        this.userDetails = userDetails;
+    public OnUserLoggedInEvent(String userId) {
+        super(userId);
+        this.userId = userId;
     }
 
-    public UserDetailsImpl getUserDetails() {
-        return userDetails;
-    }
-
-    public void setUserDetails(UserDetailsImpl userDetails) {
-        this.userDetails = userDetails;
-    }
 }
--- backend/src/main/java/com/todo/event/listener/OnUserLoggedInListener.java
+++ backend/src/main/java/com/todo/event/listener/OnUserLoggedInListener.java
@@ -2,16 +2,16 @@ package com.todo.event.listener;
 
 import com.todo.event.OnUserLoggedInEvent;
 import com.todo.repository.RefreshTokenRepository;
-import com.todo.security.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.time.Instant;
 
 @Component
-public class OnUserLoggedInListener implements ApplicationListener<OnUserLoggedInEvent> {
+public class OnUserLoggedInListener {
 
     private RefreshTokenRepository refreshTokenRepository;
 
@@ -21,22 +21,16 @@ public class OnUserLoggedInListener implements ApplicationListener<OnUserLoggedI
     }
 
     /**
-     * As soon as a logged in event is complete, invoke payload procedure in an another thread pool
-     */
-    @Override
-    @Async
-    public void onApplicationEvent(OnUserLoggedInEvent onUserLoggedInEvent) {
-        checkStaleTokensForLoggedInUser(onUserLoggedInEvent);
-    }
-
-    /**
      * Delete all expired refresh tokens for that currently logged in user.
      * U can implement your custom logic here.
      * For example, if you want user to have only 5 active refresh tokens, your can remove old ones and etc.
      */
-    private void checkStaleTokensForLoggedInUser(OnUserLoggedInEvent event) {
-        final UserDetailsImpl userDetails = event.getUserDetails();
+    @Async
+    @TransactionalEventListener
+    public void handleUserLoggedInEvent(OnUserLoggedInEvent onUserLoggedInEvent) {
+        final String userId = onUserLoggedInEvent.getUserId();
         final Long currentTimeInSeconds = Instant.now().getEpochSecond();
-        refreshTokenRepository.deleteByUserIdAndExpiredInSecondsBefore(userDetails.getId(), currentTimeInSeconds);
+        refreshTokenRepository.deleteByUserIdAndExpiredInSecondsBefore(userId, currentTimeInSeconds);
     }
+
 }
